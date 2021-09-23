48ed26a12e2d511335755faca6636838f01f07ab
Nebojsa Ristic <ris.nebojsa@gmail.com>
2021-09-23 10:08:25 +0200

    [mag-64] Fix: sequelize config

--- lib/infrastructure/config/modules/sequelize.ts
+++ lib/infrastructure/config/modules/sequelize.ts
@@ -1,13 +1,15 @@
 const fs = require('fs')
-require('dotenv').config()
-console.log('env', process.env);
+process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+
+require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` });
+
 module.exports = {
   development: {
     username: process.env.DB_USERNAME || 'mogul_user',
     password: process.env.DB_PASSWORD || 'test1234',
     database: process.env.DB_NAME || 'moguldb',
     host: process.env.DB_HOSTNAME || '127.0.0.1',
-    port: process.env.DB_PORT || 5438,
+    port: process.env.DB_PORT || 5432,
     dialect: 'postgres',
   },
   staging: {
